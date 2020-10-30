747c2338bd62f85e18a5b9f6eeee99cf67d66d8e
ABC <ati.sz@freemail.hu>
2020-10-30 08:19:55 +0100

    Integrate

---
+++ src/main/java/classsctructureintegrate/Bank.java
@@ -0,0 +1,47 @@
+package classsctructureintegrate;
+
+import java.util.Scanner;
+
+public class Bank {
+    public static void main(String[] args) {
+        System.out.println("Bank account number:");
+        Scanner scanner =new Scanner(System.in);
+        String accountNumber = scanner.nextLine();
+        System.out.println("Bank account owner:");
+        String owner = scanner.nextLine();
+        System.out.println("Bank account balance:");
+        int balance = scanner.nextInt();
+        BankAccount bankAccount = new BankAccount(accountNumber, owner, balance);
+        System.out.println("Bank account:\n" + bankAccount.getInfo());
+
+        scanner.nextLine();
+        System.out.println("Deposit:");
+        int amount = scanner.nextInt();
+        bankAccount.deposit(amount);
+        System.out.println("Bank account after deposit:\n" + bankAccount.getInfo());
+
+        scanner.nextLine();
+        System.out.println("Withdraw:");
+        amount = scanner.nextInt();
+        bankAccount.withdraw(amount);
+        System.out.println("Bank account after withdraw:\n" + bankAccount.getInfo());
+
+        scanner.nextLine();
+        System.out.println("Transfer Bank account number:");
+        accountNumber = scanner.nextLine();
+        System.out.println("Transfer Bank account owner:");
+        owner = scanner.nextLine();
+        System.out.println("Transfer Bank account balance:");
+        balance = scanner.nextInt();
+        BankAccount anotherBankAccount = new BankAccount(accountNumber, owner, balance);
+        System.out.println("Transfer Bank account:\n" + anotherBankAccount.getInfo());
+
+        scanner.nextLine();
+        System.out.println("Transfer amount:");
+        amount = scanner.nextInt();
+        bankAccount.transfer(anotherBankAccount, amount);
+        System.out.println("Bank account after transfer:\n" + bankAccount.getInfo());
+        System.out.println("Transfer Bank account after transfer:\n" + anotherBankAccount.getInfo());
+
+    }
+}
---
+++ src/main/java/classsctructureintegrate/Product.java
@@ -0,0 +1,53 @@
+package classsctructureintegrate;
+
+import java.util.Scanner;
+
+public class Product {
+    private String name;
+    private int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void increase(int price) {
+        this.price += price;
+    }
+
+    public void decrease(int price) {
+        this.price -= price;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Product name:");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        System.out.println("Product price:");
+        int price = scanner.nextInt();
+
+        Product product = new Product(name, price);
+        System.out.println("Product data:\t" + product.getName() + "\t" + product.getPrice());
+
+        System.out.println("Price increase with:");
+        int change = scanner.nextInt();
+        product.increase(change);
+        System.out.println("Product data after increase price:\t" + product.getName() + "\t" + product.getPrice());
+
+        System.out.println("Price decrease with:");
+        change = scanner.nextInt();
+        product.decrease(change);
+        System.out.println("Product data after decrease price:\t" + product.getName() + "\t" + product.getPrice());
+
+    }
+
+}
