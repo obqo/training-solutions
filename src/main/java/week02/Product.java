98ad33fefa0be0be8b9d80c653eb83f962875eb4
ABC <ati.sz@freemail.hu>
2020-11-04 16:18:45 +0100

    product

---
+++ src/main/java/week02/Product.java
@@ -0,0 +1,34 @@
+package week02;
+
+public class Product {
+    private String name;
+    private String code;
+
+    public Product(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public boolean areTheyEqual(Product prod) {
+        return name.equals(prod.getName()) && Math.abs(code.length() - prod.getCode().length()) <= 1;
+    }
+
+    public static void main(String[] args) {
+        Product product1 = new Product("alma", "00001");
+        Product product2 = new Product("körte", "01");
+        Product product3 = new Product("alma", "0120");
+
+
+        System.out.println(product1.getName() + " és " + product2.getName() + " azonosak? " + product1.areTheyEqual(product2));
+        System.out.println(product1.getName() + " és " + product3.getName() + " azonosak? " + product1.areTheyEqual(product3));
+
+    }
+}
