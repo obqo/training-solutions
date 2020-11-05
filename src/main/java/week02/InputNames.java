280ec4056a36e9bf3482ca83762b2492c5e9a454
ABC <ati.sz@freemail.hu>
2020-11-05 16:02:35 +0100

    11.05

---
+++ src/main/java/week02/InputNames.java
@@ -0,0 +1,23 @@
+package week02;
+
+import java.util.Scanner;
+
+public class InputNames {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] array = new String[5] ;
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i + 1 + ". név:");
+            array[i] = scanner.nextLine();
+        }
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i + 1 + ". név: " + array[i]);
+
+        }
+
+    }
+
+}
