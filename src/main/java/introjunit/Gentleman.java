39cdfa9cf126e604bc1921d6a14faf683f6012c3
obqo <ati.sz@freemail.hu>
2020-11-24 09:22:30 +0100

    introjunit

---
+++ src/main/java/introjunit/Gentleman.java
@@ -0,0 +1,17 @@
+package introjunit;
+
+public class Gentleman {
+
+    public String sayHello(String name) {
+        if (name.isEmpty()) {
+            return "Hello Anonymous";
+        }
+        else {
+            return "Hello " + name;
+        }
+    }
+
+    /*
+
+     */
+}
---
+++ src/test/java/introjunit/GentlemanTest.java
@@ -0,0 +1,14 @@
+package introjunit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GentlemanTest {
+
+    @Test
+    void testSayHello() {
+        assertEquals("Hello John Doe", new Gentleman().sayHello("John Doe"));
+        assertEquals("Hello Anonymous", new Gentleman().sayHello(""));
+    }
+}
