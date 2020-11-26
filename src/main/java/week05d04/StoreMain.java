509f1e6052d2bdefd021f5759791a2117e1d1920
obqo <ati.sz@freemail.hu>
2020-11-26 15:47:50 +0100

    week05d04

---
+++ src/main/java/week05d04/StoreMain.java
@@ -0,0 +1,14 @@
+package week05d04;
+
+public class StoreMain {
+
+    public static void main(String[] args) {
+
+        Store store = new Store();
+        store.addProduct(new Product("milk", 2020, 12, 1));
+        store.addProduct(new Product("ham", 2020, 11, 25));
+        store.addProduct(new Product("bread", 2020, 11, 27));
+        System.out.println(store.productList.toString());
+        System.out.println(store.getNumberOfExpired());
+    }
+}
