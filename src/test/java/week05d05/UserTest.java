eaaaaae690fa5f2156f7e56c08fc6f32ad785f45
obqo <ati.sz@freemail.hu>
2020-11-27 15:42:51 +0100

    week05d05

---
+++ src/test/java/week05d05/UserTest.java
@@ -0,0 +1,39 @@
+package week05d05;
+
+import introexception.patient.Patient;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class UserTest {
+
+    @Test
+    void testUserGood() {
+        User user = new User("Kiss", "Csaba", "a@a.com");
+        assertEquals("Kiss Csaba", user.getFullName());
+    }
+
+    @Test
+    void testUserNotDot() {
+        assertThrows(IllegalArgumentException.class, () -> new User("Kiss", "Csaba", "a@acom"));
+    }
+
+    @Test
+    void testUserNotAt() {
+        assertThrows(IllegalArgumentException.class, () -> new User("Kiss", "Csaba", "aa.com"));
+    }
+
+    @Test
+    void testUserNotAtDot() {
+        assertThrows(IllegalArgumentException.class, () -> new User("Kiss", "Csaba", "aacom"));
+    }
+
+    @Test
+    void testValidEmail() {
+        assertThrows(IllegalArgumentException.class, () -> new User("Kiss", "Csaba", "a@.acom"));
+        assertThrows(IllegalArgumentException.class, () -> new User("Kiss", "Csaba", "@a.com"));
+        assertThrows(IllegalArgumentException.class, () -> new User("Kiss", "Csaba", "a@acom."));
+        assertThrows(IllegalArgumentException.class, () -> new User("Kiss", "Csaba", "a.b@a.com"));
+    }
+}
