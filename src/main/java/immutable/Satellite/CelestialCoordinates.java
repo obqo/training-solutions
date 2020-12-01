f46dbf9dcecdef1530b556b462a7b61362b8b66f
obqo <ati.sz@freemail.hu>
2020-12-01 05:56:00 +0100

    immutable

---
+++ src/main/java/immutable/Satellite/CelestialCoordinates.java
@@ -0,0 +1,34 @@
+package immutable.Satellite;
+
+public class CelestialCoordinates {
+
+    private final long x;
+    private final long y;
+    private final long z;
+
+    public CelestialCoordinates(long x, long y, long z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public long getX() {
+        return x;
+    }
+
+    public long getY() {
+        return y;
+    }
+
+    public long getZ() {
+        return z;
+    }
+
+    @Override
+    public String toString() {
+        return "CelestialCoordinates" +
+                ": x=" + x +
+                ", y=" + y +
+                ", z=" + z;
+    }
+}
