e0fd82e05e680174a9b5ef82079f1cd37131445e
obqo <ati.sz@freemail.hu>
2020-12-09 17:02:47 +0100

    week07d03

--- src/main/java/week07d03/NumberList.java
+++ src/main/java/week07d03/NumberList.java
@@ -1,3 +1,8 @@
+/*
+A week07d03 csomagban hozz létre egy NumberList osztályt. Ennek legyen egy metódusa isIncreasing(List<Integer>) mely egy számokból álló
+listát vár paraméterül és megnézi, hogy a listában a számok növekvő sorrendben szerepelnek-e és ennek megfelelően igaz vagy hamis
+értékkel tér vissza. Speciális eset ha két egymást követő szám egyenlő, ez nem probléma a 1,2,3,3,3,5 számokat növekvőnek tekintjük.
+ */
 package week07d03;
 
 import java.util.Arrays;
--- src/test/java/week07d03/NumberListTest.java
+++ src/test/java/week07d03/NumberListTest.java
@@ -10,11 +10,11 @@ public class NumberListTest {
 
     @Test
     public void testNumberList() {
+        assertThrows(IllegalArgumentException.class, () -> NumberList.isIncreasing(null));
         assertTrue(NumberList.isIncreasing(Arrays.asList(1, 2, 3, 3, 5)));
         assertFalse(NumberList.isIncreasing(Arrays.asList(1, 2, 4, 3, 5)));
         assertTrue(NumberList.isIncreasing(Arrays.asList(1, 2)));
         assertFalse(NumberList.isIncreasing(Arrays.asList(1, -1)));
         assertTrue(NumberList.isIncreasing(Arrays.asList(1)));
-        assertThrows(IllegalArgumentException.class, () -> NumberList.isIncreasing(null));
     }
 }
