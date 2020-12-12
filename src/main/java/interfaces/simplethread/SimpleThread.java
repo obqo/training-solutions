32f8d9e8d3937b4cb2a7a32634aab183a6e7c78f
obqo <ati.sz@freemail.hu>
2020-12-12 14:03:46 +0100

    interfaces.simplethread

---
+++ src/main/java/interfaces/simplethread/SimpleThread.java
@@ -0,0 +1,31 @@
+package interfaces.simplethread;
+
+import java.util.List;
+
+public class SimpleThread implements Runnable{
+
+    private List<String> tasks;
+
+    public SimpleThread(List<String> tasks) {
+        this.tasks = tasks;
+    }
+
+    public List<String> getTasks() {
+        return tasks;
+    }
+
+    private boolean nextStep() {
+        if (tasks != null && !tasks.isEmpty()) {
+            tasks.remove(tasks.size() - 1);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void run() {
+        while (nextStep()) {
+
+        }
+    }
+}
