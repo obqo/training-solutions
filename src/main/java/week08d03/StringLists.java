05106dbb203fab5e49fbb4af05fb1e9326cad6a0
obqo <ati.sz@freemail.hu>
2020-12-16 15:40:47 +0100

    week08d03 mid-sen

---
+++ src/main/java/week08d03/StringLists.java
@@ -0,0 +1,64 @@
+/*
+Készíts week08d03 csomagban egy StringLists osztályt és benne egy stringListsUnion(List<String> first, List<String> second) metódust,
+mely két string listát vár paraméterül és visszaadja az uniójukat. Az úniónak minden elemet tartalmaznia kell mindkét listából,
+de egy elem nem szerepelhet kétszer!
+ */
+package week08d03;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StringLists {
+
+    public static List<String> stringListUnion(List<String> first, List<String> second) {
+        List<String> unionList = new ArrayList<>();
+        if (!isEmpty(first)) {
+            unionList.addAll(first);
+        }
+        if (!isEmpty(second)) {
+            unionList.addAll(second);
+        }
+        return removeDuplication(unionList);
+    }
+
+    private static List<String> removeDuplication(List<String> list) {
+        List<String> newList = new ArrayList<>();
+        for (String string : list) {
+            if (!newList.contains(string)) {
+                newList.add(string);
+            }
+        }
+        return newList;
+    }
+/*
+Készíts a week08d03 csomagban egy StringLists osztályt és benne egy shortestWords(List<String> words) metódust, mely paraméterül kap egy String listát
+és a legrövidebb szavakat vissza adja belőle egy Listában. PL :{"aaa", "aa", "bb", "cccc", "dd"} Itt a legrövidebb szavak az "aa", "bb", "dd".
+ */
+    public static List<String>shortestWords(List<String> words) {
+        List<String> newList = new ArrayList<>();
+        if (!isEmpty(words)) {
+            int lengthMin = minLength(words);
+            for (String item : words) {
+                if (item.length() == lengthMin) {
+                    newList.add(item);
+                }
+            }
+        }
+        return newList;
+    }
+
+    private static int minLength(List<String> list) {
+        int min = list.get(0).length();
+        for (String item : list) {
+            if (item.length() < min) {
+                min = item.length();
+            }
+        }
+        return min;
+    }
+
+    private static boolean isEmpty(List<String> list) {
+        return list == null || list.isEmpty();
+    }
+}
---
+++ src/test/java/week08d03/StringListsTest.java
@@ -0,0 +1,46 @@
+package week08d03;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringListsTest {
+
+    @Test
+    void testStringListsNoEmpty() {
+        assertEquals("[1, 2, 3, 4]", StringLists.stringListUnion(
+                new ArrayList<>(Arrays.asList("1", "2", "3", "1")),
+                new ArrayList<>(Arrays.asList("1", "4"))).toString());
+    }
+
+    @Test
+    void testStringListsEmpty() {
+        assertEquals("[1, 4]", StringLists.stringListUnion(new ArrayList<>(), new ArrayList<>(Arrays.asList("1", "4"))).toString());
+        assertEquals("[1, 2, 3]", StringLists.stringListUnion(new ArrayList<>(Arrays.asList("1", "2", "3", "1")), new ArrayList<>()).toString());
+    }
+
+    @Test
+    void testStringListsNull() {
+        assertEquals("[1, 2, 3]", StringLists.stringListUnion(new ArrayList<>(Arrays.asList("1", "2", "3", "1")), null).toString());
+        assertEquals("[1, 4]", StringLists.stringListUnion(null, new ArrayList<>(Arrays.asList("1", "4"))).toString());
+        assertEquals("[]", StringLists.stringListUnion(null, null).toString());
+    }
+
+    @Test
+    void testShotestWordsNoEmpty () {
+        assertEquals("[aa, bb, dd]", StringLists.shortestWords(new ArrayList<>(Arrays.asList("aaa", "aa", "bb", "cccc", "dd"))).toString());
+    }
+
+    @Test
+    void testShotestWordsEmpty () {
+        assertEquals("[]", StringLists.shortestWords(new ArrayList<>()).toString());
+    }
+
+    @Test
+    void testShotestWordsNull () {
+        assertEquals("[]", StringLists.shortestWords(null).toString());
+    }
+}
