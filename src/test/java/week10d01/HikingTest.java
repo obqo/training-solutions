0ab21e815a54190a66346cd4d6ba825608a931b9
obqo <ati.sz@freemail.hu>
2021-01-04 15:35:57 +0100

    week10d01-mid

---
+++ src/test/java/week10d01/HikingTest.java
@@ -0,0 +1,19 @@
+package week10d01;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class HikingTest {
+
+    @Test
+    public void testHiking() {
+        assertEquals(13.0, Hiking.getPlusElevation(List.of(10.0, 20.0, 15.0, 18.0)), 0.0001);
+        assertEquals(0.0, Hiking.getPlusElevation(List.of(10.0)), 0.0001);
+        assertEquals(0.0, Hiking.getPlusElevation(List.of()), 0.0001);
+        assertThrows(IllegalArgumentException.class, () -> Hiking.getPlusElevation(null));
+    }
+}
