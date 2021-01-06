d0f41b86608cfa31b0365f51ba47d6deafe60b65
obqo <ati.sz@freemail.hu>
2021-01-06 19:00:04 +0100

    exam02-End

---
+++ src/main/java/exam02/ArraySelector.java
@@ -0,0 +1,29 @@
+package exam02;
+
+import a.A;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArraySelector {
+
+    public String selectEvens(int[] array) {
+        if (array == null) {
+            throw new IllegalArgumentException("List is null");
+        }
+        List<Integer> evenIndexList = new ArrayList<>();
+        for (int i = 0; i < array.length; i = i + 2) {
+            evenIndexList.add(array[i]);
+        }
+        if (evenIndexList.toString() == "[]") {
+            return "";
+        }
+        return evenIndexList.toString();
+    }
+
+    public static void main(String[] args) {
+        A a = new A();
+        int[] array = {123, 21, 11, 15};
+        System.out.println(a.evenArrayElements(array));
+    }
+}
---
+++ src/main/java/exam02/cv/Cv.java
@@ -0,0 +1,47 @@
+package exam02.cv;
+
+import a.C;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cv {
+
+    private String name;
+    private List<Skill> skills = new ArrayList();
+
+    public Cv(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Skill> getSkills() {
+        return skills;
+    }
+
+    public void addSkills(String ... strings) {
+        for (String string : strings) {
+            String skillName = string.substring(0, string.indexOf(" ("));
+            String skillLevel = string.substring(string.indexOf(" (") + 2, string.indexOf(" (") + 3);
+            this.skills.add(new Skill(skillName, Integer.parseInt(skillLevel)));
+        }
+    }
+
+    public int findSkillLevelByName(String skillName) {
+        int result = 0;
+        for (Skill skill : skills) {
+            if (skill.getName().equals(skillName)) {
+                result = skill.getLevel();
+            }
+        }
+        if (result ==0) {
+            throw new SkillNotFoundException();
+        }
+        return result;
+    }
+
+
+}
