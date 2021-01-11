2c005e0bd9dc3504194633488218b41152c8ab44
obqo <ati.sz@freemail.hu>
2021-01-11 13:31:07 +0100

    week11d01-mid

--- src/main/java/week11/week11d01/DivisorFinder.java
+++ src/main/java/week11/week11d01/DivisorFinder.java
@@ -1,3 +1,8 @@
+/*
+Készítsünk egy DivisorFinder nevű osztályt, melynek van egy int findDivisors(int n) metódusa. A feladat az, hogy
+megnézzük a szám minden egyes számjegyére, hogy osztója-e a számnak, majd számoljuk össze őket. Példa: a 425-ben
+az 5 osztója a számnak, ezért a visszatérési érték 1.
+ */
 package week11.week11d01;
 
 public class DivisorFinder {
--- src/test/java/week11/week11d01/DivisorFinderTest.java
+++ src/test/java/week11/week11d01/DivisorFinderTest.java
@@ -13,7 +13,7 @@ class DivisorFinderTest {
         assertEquals(0, divisorFinder.findDivisors(29));
         assertEquals(2, divisorFinder.findDivisors(15));
         assertEquals(5, divisorFinder.findDivisors(-33555));
-        assertEquals(3, divisorFinder.findDivisors(4155));
+        assertEquals(3, divisorFinder.findDivisors(4115));
         assertEquals(0, divisorFinder.findDivisors(0));
     }
 }
