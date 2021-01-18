2bffc60f83c9c4e4dd27a0bc398c5c8750664acf
obqo <ati.sz@freemail.hu>
2021-01-18 15:45:13 +0100

    week12d01-mid

---
+++ src/main/java/week12/week12d01/GradeRounder.java
@@ -0,0 +1,27 @@
+/*
+Készíts egy GradeRounder osztályt, amiben van egy int[] roundGrades(int[] grades) metódus. A grades tömb pontszámokat tartalmaz 0 és 100 között.
+A feladat az, hogy kerekítsük a benne lévő számokat a következő szabály szerint: Ha a pontszám és az 5 következő többszöröse közötti különbség kisebb,
+mint 3, akkor kerekítsük fel a számot az 5 következő többszörösére. Fontos: a 40 pont alatti pontszámok elégtelenek, ezeket egyáltalán nem kell
+kerekíteni. Példa: a pontszám 84. 85 - 84 kevesebb mint 3, így felfelé kerekítünk.
+ */
+package week12.week12d01;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class GradeRounder {
+
+    public int[] roundGrades(int[] grades) {
+        if (grades == null) {
+            throw new IllegalArgumentException("No list");
+        }
+        int[] result = new int[grades.length];
+        for (int i = 0; i < grades.length; i++) {
+            result[i] = grades[i] % 5 < 3 ? grades[i] : (grades[i] / 5 + 1) * 5;
+        }
+        return result;
+    }
+}
---
+++ src/test/java/week12/week12d01/GradeRounderTest.java
@@ -0,0 +1,26 @@
+package week12.week12d01;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GradeRounderTest {
+
+    GradeRounder gradeRounder = new GradeRounder();
+
+    @Test
+    void roundGrades() {
+        int[] grades = new int[]{1, 3, 4, 5, 6, 7, 84};
+        String result = "[1, 5, 5, 5, 6, 7, 85]";
+        assertEquals(result, Arrays.stream(gradeRounder.roundGrades(grades)).boxed().collect(Collectors.toList()).toString());
+    }
+
+    @Test
+    void roundGradesNoNumbers() {
+        assertThrows(IllegalArgumentException.class, () -> gradeRounder.roundGrades(null));
+    }
+}
