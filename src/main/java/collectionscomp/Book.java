277e448d43e39c5f2bf69d22e8fd50bc0c4289dd
obqo <ati.sz@freemail.hu>
2021-01-30 19:12:59 +0100

    collectionscomp

---
+++ src/main/java/collectionscomp/Book.java
@@ -0,0 +1,31 @@
+package collectionscomp;
+
+public class Book implements Comparable<Book> {
+
+    private final int regNumber;
+    private final String title;
+    private final String author;
+
+    public Book(int regNumber, String title, String author) {
+        this.regNumber = regNumber;
+        this.title = title;
+        this.author = author;
+    }
+
+    public int getRegNumber() {
+        return regNumber;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public int compareTo(Book book) {
+        return title.compareTo(book.getTitle());
+    }
+}
---
+++ src/main/java/collectionscomp/OrderedLibrary.java
@@ -0,0 +1,59 @@
+/*
+Készítsünk olyan osztályt, ami egy könyvtárban tárolt könyvek szoftveres menedzselését valósítja meg.
+A könyvtárat feltöltjük könyvekkel, és kereséshez cím vagy szerő szerint rendezhetjük a könyveket.
+Alapértelmezett a cím szerinti rendezés. Egy külön metódusban valósítsuk meg a könyvcímek magyar
+ékezetes karaktereket is figyelembe vevő rendezését. Csak a könyvcímek kellenek, nem a könyvek!
+Megvalósítás
+Book osztály int regNumber String title és String author attribútumokkal.
+A public int compareTo(Book o) metódust az igényeknek megfelelően készítsük el.
+OrderedLibrary osztály List<Book> libraryBooks attribútummal. Ezt konstruktorból tudjuk feltölteni.
+publikus metódusok:
+ public OrderedLibrary(List<Book> libraryBooks)
+ public List<Book> orderedByTitle()
+ public List<Book> orderedByAuthor()
+ public List<String> orderedByTitleLocale(Locale locale)
+Egy külön osztályban (AuthorComparator) valósítsuk meg a szerző szerint történő rendezéshez szükséges Comparator osztályt.
+Bónusz feladat - Collator
+Az ékezetes karaktereket is figyelembe vevő String rendezéshez a Collator osztály egy példányára lesz szükségünk.
+Nézz utána, ez hogyan állítható elő és hogyan paraméterezhető!
+Bónusz feladat - Comparator
+A Comparator objektumot többnyire névtelen osztály példányaként állítjuk elő. Nézz utána, ez hogyan valósítható meg!
+ */
+package collectionscomp;
+
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+public class OrderedLibrary {
+
+    private final List<Book> libraryBooks;
+
+    public OrderedLibrary(List<Book> libraryBooks) {
+        this.libraryBooks = libraryBooks;
+    }
+
+    public List<Book> orderedByTitle() {
+        List<Book> sortedBooksByTitle = new ArrayList<>(libraryBooks);
+        Collections.sort(sortedBooksByTitle);
+        return sortedBooksByTitle;
+    }
+
+    public List<Book> orderedByAuthor() {
+        List<Book> sortedBooksByAuthor = new ArrayList<>(libraryBooks);
+        Collections.sort(sortedBooksByAuthor, new AuthorComparator());
+        return sortedBooksByAuthor;
+    }
+
+    public List<String> orderedByTitleLocale(Locale locale) {
+        List<String> sortedBooksByTitleLocale = new ArrayList<>();
+        for (Book book : libraryBooks) {
+            sortedBooksByTitleLocale.add(book.getTitle());
+        }
+        Collections.sort(sortedBooksByTitleLocale, Collator.getInstance(locale));
+        return sortedBooksByTitleLocale;
+    }
+
+}
