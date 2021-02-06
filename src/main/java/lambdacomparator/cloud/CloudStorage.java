588872f16ceb82df7165f59ba12adcd013a601a6
obqo <ati.sz@freemail.hu>
2021-02-06 17:38:54 +0100

    lambdacomparator

---
+++ src/main/java/lambdacomparator/cloud/CloudStorage.java
@@ -0,0 +1,44 @@
+package lambdacomparator.cloud;
+
+public class CloudStorage implements Comparable<CloudStorage>{
+
+    private final String provider;
+    private final int space;
+    private PayPeriod period;
+    private double price;
+
+    public CloudStorage(String provider, int space, PayPeriod period, double price) {
+        this.provider = provider;
+        this.space = space;
+        this.period = period;
+        this.price = price;
+    }
+
+    public CloudStorage(String provider, int space) {
+        this.provider = provider;
+        this.space = space;
+    }
+
+    public String getProvider() {
+        return provider;
+    }
+
+    public int getSpace() {
+        return space;
+    }
+
+    public PayPeriod getPeriod() {
+        return period;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public int compareTo(CloudStorage o) {
+        double currentValue = this.period != null ? this.price * 12 / this.period.getLength() / this.space * 1000 : 0;
+        double otherValue = o.period != null ? o.price * 12 / o.period.getLength() / o.space * 1000 : 0;
+        return Double.compare(currentValue, otherValue);
+    }
+}
---
+++ src/main/java/lambdacomparator/cloud/Clouds.java
@@ -0,0 +1,39 @@
+/*
+Különböző felhő tárhely szolgáltatókat szeretnénk összehasonlítani, ezért a CloudStorage osztályban eltároljuk a
+különböző adataikat. A tárhely mérete GB-ban adott, az árak pedig mindenhol ugyanabban a pénznemben.
+A PayPeriod enum a fizetési gyakoriság, ahol a length értéke a hossz hónapokban megadva (lifetime esetén 60 hónap).
+Az ingyenes csomagok esetén a fizetési gyakortiság nincs megadva. A CloudStorage implementálja a Comparable interfészt,
+a természetes rendezettségét az 1000 GB-ra eső éves díj nagysága adja.
+A Clouds osztály metódusai a paraméterként kapott listából bizonyos szempont szerint a legjobba(ka)t adják vissza.
+Amennyiben több ugyanolyan van, akkor közülük bármelyik visszaadható.
+alphabeticallyFirst(): a szolgáltató neve alapján betűrendben a legelső CloudStorage. Kis-nagybetű nem számít.
+bestPriceForShortestPeriod(): a legrövidebb időszakra vonatkozó legolcsóbb CloudStorage.
+Ha van ingyenes, akkor azok közül bármelyik megadható.
+worstOffers(): a természetes rendezettség szerinti 3 legrosszabb ajánlat.
+ */
+package lambdacomparator.cloud;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Clouds {
+
+    public CloudStorage alphabeticallyFirst(List<CloudStorage> storages) {
+        storages.sort(Comparator.comparing(CloudStorage::getProvider, String::compareToIgnoreCase));
+        return storages.get(0);
+    }
+
+    public CloudStorage bestPriceForShortestPeriod(List<CloudStorage> storages) {
+        return Collections.min(storages, Comparator.comparing(CloudStorage::getPeriod,
+                Comparator.nullsFirst(Comparator.comparingInt(PayPeriod::getLength)))
+                .thenComparingDouble(CloudStorage::getPrice));
+    }
+
+    public List<CloudStorage> worstOffers(List<CloudStorage> storages) {
+        List<CloudStorage> copyStorages = new ArrayList<>(storages);
+        copyStorages.sort(Comparator.reverseOrder());
+        return copyStorages.subList(0, Math.min(3, copyStorages.size()));
+    }
+}
