9f1c9e5f4d0c6956ed15aff1daece06c051ab3cf
obqo <ati.sz@freemail.hu>
2021-02-14 07:00:48 +0100

    recursion

---
+++ src/main/java/recursion/FactorialCalculator.java
@@ -0,0 +1,12 @@
+package recursion;
+
+public class FactorialCalculator {
+
+    public long getFactorial(int number) {
+        if (number > 1) {
+            return number * getFactorial(number - 1);
+        } else {
+            return 1;
+        }
+    }
+}
