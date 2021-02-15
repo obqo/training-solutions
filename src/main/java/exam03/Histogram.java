2c2b5ac001c81f1631871c9231318ac020a3a2f8
obqo <ati.sz@freemail.hu>
2021-02-15 18:20:13 +0100

    end-Histogram

--- src/main/java/exam03/Histogram.java
+++ src/main/java/exam03/Histogram.java
@@ -1,4 +1,33 @@
+/*
+Olvass be egy szöveges fájlt soronként, melyben soronkont számok vannak. Majd hozz létre egy stringet,
+mely ugyanennyi sort tartalmaz, és annyi csillagot ír ki egymás mellé soronkont, amennyi a bemeneti fájlban lévő szám.
+Azaz a bemeneti fájl:
+3
+4
+1
+2
+A létrehozott String:
+***
+****
+*
+**
+Figyelj, hogy az algoritmusod lehetőleg hatékony legyen!
+A Histogram osztályba dolgozz, tesztje HistogramTest. Minden sor után legyen sortörés, az utolsó után is.
+ */
 package exam03;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class Histogram {
+
+    public String createHistogram (BufferedReader reader) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            int nr = Integer.parseInt(line);
+            sb.append("*".repeat(nr)).append("\n");
+        }
+        return sb.toString();
+    }
 }
