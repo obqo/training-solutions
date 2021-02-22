468740f4f6aeba4d60e948608211b4650669cc97
obqo <ati.sz@freemail.hu>
2021-02-22 20:00:58 +0100

    exam03retake01-end-OwlCounter

---
+++ src/main/java/exam03retake01/OwlCounter.java
@@ -0,0 +1,33 @@
+/*
+A OwlCounter oszály egy Map<String, Integer> attribútumban tárolja,
+hogy melyik megyében mennyi baglyot számoltak a madárszámlálók. A readFromFile() metódussal (paraméterezése,
+visszatérési értéke kiderül a tesztesetből) töltsd be az értékeket egy fájlból! A fájl formátuma a következő:
+Bács-Kiskun=1000
+Baranya=1050
+Békés=980
+Borsod-Abaúj-Zemplén=600
+A getNumberOfOwls() metódus adja vissza a baglyok számát a paraméterként átadott megyében!
+ */
+package exam03retake01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class OwlCounter {
+
+    private Map<String, Integer> numberOfOwls = new HashMap<>();
+
+    public void readFromFile(BufferedReader reader) throws IOException {
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] splitLine = line.split("=");
+            numberOfOwls.put(splitLine[0], Integer.parseInt(splitLine[1]));
+        }
+    }
+
+    public int getNumberOfOwls(String key) {
+        return numberOfOwls.get(key);
+    }
+}
